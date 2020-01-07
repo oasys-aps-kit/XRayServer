5043d70dc61fd20d9a80e4074e959bfe8364c8ca
lucarebuffi <lrebuffi@anl.gov>
2020-01-07 14:27:07 -0600

    PyQt 5.14 bug fixed

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'OASYS1-XRayServer'
-VERSION = '1.0.31'
+VERSION = '1.0.32'
 ISRELEASED = True
 
 DESCRIPTION = 'X-Ray Server: Sergey Stepanov\'s X-Ray Server on OASYS'
@@ -40,8 +40,8 @@ SETUP_REQUIRES = (
 )
 
 INSTALL_REQUIRES = (
-    'oasys1>=1.2.26',
-    'PyQtWebEngine>=5.13'
+    'oasys1>=1.2.28',
+    'PyQtWebEngine>=5.14'
 )
 
 PACKAGES = find_packages(exclude=('*.tests', '*.tests.*', 'tests.*', 'tests'))
